c4b16f0772c67be071761f6cf83593203057d9b5
Vitalina5 <viktoriana15.com>
2022-07-03 22:32:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const boxRef = useRef();
 
   const fetchTips = async () => {
-    const response = await fetch('http://www.boredapi.com/api/activity/');
+    const response = await fetch('https://www.boredapi.com/api/activity/');
     const data = await response.json();
     setTips(data.activity);
   }
