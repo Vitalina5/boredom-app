28711c5d8531c53867217dc0e5d6f5ef193dea15
Vitalina5 <viktoriana15.com>
2022-07-03 12:15:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState, useRef } from 'react';
+import { gsap } from "gsap";
 import './App.css';
+import image from './arrow.png';
+
 
 function App() {
-  return (
+  const [tips, setTips] = useState('');
+
+  const boxRef = useRef();
+
+  const fetchTips = async () => {
+    const response = await fetch('http://www.boredapi.com/api/activity/');
+    const data = await response.json();
+    setTips(data.activity);
+  }
+
+  useEffect(() => {
+    fetchTips();
+  }, []);
+
+  useEffect(() => {
+    gsap.to(boxRef.current, {y: 55, ease: "elastic", delay: 0.5, duration: 4, repeat: -1});
+  },[]);
+  
+
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='cont'>
+        <h1>What should I do today?</h1>
+      </div>
+      <div className='cont'>
+        <img className='arrow' ref={boxRef} src={image} alt='arrow' width='150px'/>
+      </div>
+      <div className='cont'>
+        <h2>{tips}</h2>
+      </div>
+      <div className='cont'>
+        <button className='btn' onClick={fetchTips}>NEW TIP</button>
+      </div>
     </div>
   );
 }
